51880f49f2b13c7bc42d06b3d06b2c6b560e50d9
David B <37987466+dalijolijo@users.noreply.github.com>
2018-06-20 14:18:36 +0200

    Changes for BTX

--- test/index.js
+++ test/index.js
@@ -1,27 +1,27 @@
 'use strict';
 
 var should = require('chai').should();
-var bitcore = require('../');
+var btxcore = require('../');
 
 describe('Library', function() {
   it('should export primatives', function() {
-    should.exist(bitcore.crypto);
-    should.exist(bitcore.encoding);
-    should.exist(bitcore.util);
-    should.exist(bitcore.errors);
-    should.exist(bitcore.Address);
-    should.exist(bitcore.Block);
-    should.exist(bitcore.MerkleBlock);
-    should.exist(bitcore.BlockHeader);
-    should.exist(bitcore.HDPrivateKey);
-    should.exist(bitcore.HDPublicKey);
-    should.exist(bitcore.Networks);
-    should.exist(bitcore.Opcode);
-    should.exist(bitcore.PrivateKey);
-    should.exist(bitcore.PublicKey);
-    should.exist(bitcore.Script);
-    should.exist(bitcore.Transaction);
-    should.exist(bitcore.URI);
-    should.exist(bitcore.Unit);
+    should.exist(btxcore.crypto);
+    should.exist(btxcore.encoding);
+    should.exist(btxcore.util);
+    should.exist(btxcore.errors);
+    should.exist(btxcore.Address);
+    should.exist(btxcore.Block);
+    should.exist(btxcore.MerkleBlock);
+    should.exist(btxcore.BlockHeader);
+    should.exist(btxcore.HDPrivateKey);
+    should.exist(btxcore.HDPublicKey);
+    should.exist(btxcore.Networks);
+    should.exist(btxcore.Opcode);
+    should.exist(btxcore.PrivateKey);
+    should.exist(btxcore.PublicKey);
+    should.exist(btxcore.Script);
+    should.exist(btxcore.Transaction);
+    should.exist(btxcore.URI);
+    should.exist(btxcore.Unit);
   });
 });
