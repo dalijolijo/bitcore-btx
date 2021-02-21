e7f646107f933780e6a2638c6e61760420ea4629
dalijolijo <dalijolijo@github.com>
2021-02-21 12:36:34 +0100

    Add telegram notification and cleanup

--- cleanup.md
+++ cleanup.md
@@ -5,6 +5,9 @@ docker-compose down
 rm -rf /home/bitcore
 rm -rf /home/mongodb/
 docker rmi bitcored:latest
+sync; echo 1 > /proc/sys/vm/drop_caches
 docker system prune -f
+mkdir /home/bitcore
+cp -R /home/bootstrap/* /home/bitcore/
 docker-compose up -d
 ```
---
+++ telegram_notification.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#set -x
+
+# cron job config
+# crontab -e
+#*/10 * * * * /root/insight/bitcore-btx/telegram_notification.sh > /var/log/telegram_notificationy.log 2>&1
+
+usage()
+{
+   echo "Usage:"
+   echo "    telegram_notification.sh -h                    Display this help message"
+}
+
+while getopts ":h" opt; do
+  case ${opt} in
+    h )
+      usage
+      exit 0
+      ;;
+   \? )
+     echo "Invalid Option: -$OPTARG" 1>&2
+     exit 1
+     ;;
+  esac
+done
+shift $((OPTIND -1))
+
+dpkg-query -l python3-pip
+if [ $? -ne 0 ] ; then
+   apt-get install -y python3-pip
+fi
+#Telegram CLI
+# git: https://github.com/erayerdin/tgcli
+# doku: https://tgcli.readthedocs.io/en/master
+pip3 list | grep -F tgcli
+if [ $? -ne 0 ] ; then
+   pip3 install tgcli
+fi
+
+pushd /root/insight/bitcore-btx/ >/dev/null
+source .env_telegram
+
+CHECK_RESTART=$(docker ps | grep "Restarting")
+    if [ -n "$CHECK_RESTART" ]; then
+        ERROR_MSG="The bitcore-node docker container restarts."
+        TELEGRAM_MSG="$CHECK_SERVER : $ERROR_MSG"
+        /usr/local/bin/tgcli bot -t "$MN_TELEGRAM_BOT_TOKEN" send -r "$MN_TELEGRAM_USER_ID" message "$TELEGRAM_MSG"
+    fi
+
+CHECK_EXIST_INSIGHT=$(docker ps | grep "insight")
+    if [ -z "$CHECK_EXIST_INSIGHT" ]; then
+        ERROR_MSG="The insight docker container is down."
+        TELEGRAM_MSG="$CHECK_SERVER : $ERROR_MSG"
+        /usr/local/bin/tgcli bot -t "$MN_TELEGRAM_BOT_TOKEN" send -r "$MN_TELEGRAM_USER_ID" message "$TELEGRAM_MSG"
+    fi
+
+CHECK_EXIST_BTX=$(docker ps | grep "bitcored")
+    if [ -z "$CHECK_EXIST_BTX" ]; then
+        ERROR_MSG="The bitcored docker container is down"
+        TELEGRAM_MSG="$CHECK_SERVER : $ERROR_MSG"
+        /usr/local/bin/tgcli bot -t "$MN_TELEGRAM_BOT_TOKEN" send -r "$MN_TELEGRAM_USER_ID" message "$TELEGRAM_MSG"
+    fi
+
+CHECK_EXIST_BITCORE=$(docker ps | grep "bitcore-node")
+    if [ -z "$CHECK_EXIST_BITCORE" ]; then
+        ERROR_MSG="The bitcore-node docker container is down"
+        TELEGRAM_MSG="$CHECK_SERVER : $ERROR_MSG"
+        /usr/local/bin/tgcli bot -t "$MN_TELEGRAM_BOT_TOKEN" send -r "$MN_TELEGRAM_USER_ID" message "$TELEGRAM_MSG"
+    fi
+
+CHECK_EXIST_DB=$(docker ps | grep "mongodb")
+    if [ -z "$CHECK_EXIST_DB" ]; then
+        ERROR_MSG="The bitcore-node docker container is down"
+        TELEGRAM_MSG="$CHECK_SERVER : $ERROR_MSG"
+        /usr/local/bin/tgcli bot -t "$MN_TELEGRAM_BOT_TOKEN" send -r "$MN_TELEGRAM_USER_ID" message "$TELEGRAM_MSG"
+    fi
+
+#Cleanup memory
+sync; echo 1 > /proc/sys/vm/drop_caches
